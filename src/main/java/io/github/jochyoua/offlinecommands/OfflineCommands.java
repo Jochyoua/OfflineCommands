84a5bd50c05371064ee3b575a3b2e64677cbc516
Joshua <Joshua@fairy.land>
2023-08-15 01:31:27 -0500

    enhancement: Update OfflineCommands plugin functionality
    Register additional classes for configuration serialization using YamlConfiguration
    Add variable to store user-specific data in a UserConfigData object
    Implement onReload() method for config and user data reload using reloadConfig() and loadUserData()
    Add onDisable() method for cleanup and unregistration using unregisterAll()

--- src/main/java/io/github/jochyoua/offlinecommands/OfflineCommands.java
+++ src/main/java/io/github/jochyoua/offlinecommands/OfflineCommands.java
@@ -2,14 +2,40 @@ package io.github.jochyoua.offlinecommands;
 
 import io.github.jochyoua.offlinecommands.commands.OfflineCommandExecutor;
 import io.github.jochyoua.offlinecommands.listeners.PlayerConnectionListener;
+import io.github.jochyoua.offlinecommands.storage.CommandStorage;
+import io.github.jochyoua.offlinecommands.storage.SoundStorage;
+import io.github.jochyoua.offlinecommands.storage.UserConfigData;
+import io.github.jochyoua.offlinecommands.storage.UserStorage;
+import org.bukkit.Bukkit;
 import org.bukkit.command.PluginCommand;
+import org.bukkit.configuration.serialization.ConfigurationSerialization;
+import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.java.JavaPlugin;
 
 
 public final class OfflineCommands extends JavaPlugin {
 
+    static {
+        ConfigurationSerialization.registerClass(UserStorage.class);
+        ConfigurationSerialization.registerClass(CommandStorage.class);
+        ConfigurationSerialization.registerClass(SoundStorage.class);
+    }
+
+    UserConfigData userConfigData;
+
+
+    public UserConfigData getUserStorageData() {
+        return this.userConfigData;
+    }
+
+    public void onReload() {
+        this.reloadConfig();
+        this.userConfigData.reloadConfig();
+    }
+
     @Override
     public void onEnable() {
+
         getConfig().options().copyDefaults(true);
         getConfig().options().header("OfflineCommands by Jochyoua \n Github: https://github.com/Jochyoua/OfflineCommands");
         saveDefaultConfig();
@@ -18,11 +44,26 @@ public final class OfflineCommands extends JavaPlugin {
 
         OfflineCommandExecutor offlineCommandExecutor = new OfflineCommandExecutor(this);
         PluginCommand offlineCommand = getCommand("offlinecommands");
+
         if (offlineCommand != null) {
             offlineCommand.setExecutor(offlineCommandExecutor);
             offlineCommand.setTabCompleter(offlineCommandExecutor);
         } else {
-            OfflineCommandsUtils.logMessage("Command 'offlinecommands' was unsuccessfully registered/null, perhaps a corrupt plugin.yml?", "error");
+            getLogger().warning("Command 'offlinecommands' was unsuccessfully registered/null, perhaps a corrupt plugin.yml?");
         }
+
+        userConfigData = new UserConfigData(this);
+    }
+
+    @Override
+    public void onDisable() {
+        this.userConfigData = null;
+        HandlerList.unregisterAll(this);
+        Bukkit.getScheduler().cancelTasks(this);
+
+        ConfigurationSerialization.unregisterClass(UserStorage.class);
+        ConfigurationSerialization.unregisterClass(CommandStorage.class);
+        ConfigurationSerialization.unregisterClass(SoundStorage.class);
+
     }
 }
